b2bae062352aedf449d152e4d3350e0346b916bb
SamuvelRaja <josephsamuvel001@gmail.com>
2024-07-22 22:03:53 +0530

    added header

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,20 +4,28 @@ import lib from "/src/assets/lib.svg"
 import plus from "/src/assets/plus.svg"
 
 const Sidebar = () => (
-  <nav className="max-w-[420px] min-w-[280px] p-2 ">
-    <div className="flex gap-4 flex-col ntop bg-primary-bg p-8 mb-2 rounded-md ">
-      <div className="flex  gap-4 nav-txt "><Home/> <span className=" font-medium tran-1 text-secondary-text "> Home</span></div>
-      <div className="flex  gap-4 nav-txt"><Search/> <span className=" font-medium tran-1 text-secondary-text ">Search</span></div>
+  <nav className="max-w-[420px] min-w-[280px] lg:w-[25%] p-2 ">
+    <div className="flex gap-6 flex-col ntop bg-primary-bg p-8 mb-2 rounded-md ">
+      <div className="flex  gap-6 nav-txt "><Home/> <span className=" font-medium tran-1 text-secondary-text "> Home</span></div>
+      <div className="flex  gap-6 nav-txt"><Search/> <span className=" font-medium tran-1 text-secondary-text ">Search</span></div>
     </div>
-    <div className="flex flex-col nbottom  bg-primary-bg rounded-md">
-      <div className="lib-top items-center justify-between flex p-1">
-        <div className="p-4 gap-4 flex nav-txt">
+    <div className="flex flex-col nbottom  bg-primary-bg rounded-md p-[6px]">
+      <div className="lib-top items-center justify-between flex mb-6">
+        <div className="p-4 gap-6 flex nav-txt">
           <img src={lib} className="w-[24px] h-[24px]" alt=""  />
           <span className="font-medium tran-1 text-secondary-text ">Your Library</span>
         </div>
         <div className="lib-actions pr-4">
           <div className="hover:rounded-full p-1 hover:bg-tertiary-bg w-[24px] h-[24px] my-auto tran-1"><img src={plus} className="w-[20px] " alt="" /></div>
         </div>
+      </div>
+
+      <div className="add-playlist p-4 rounded-md bg-tertiary-bg">
+        <h2 className="font-semibold text-[16px] ">Create your first playlist</h2>
+        <p className="font-thin text-[14px] mt-2">
+          It's easy, we'll help you
+        </p>
+        <button className="btn-white mt-6"> Create playlist</button>
       </div>
     </div>
   </nav>
--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -1,5 +1,22 @@
+import left from "/src/assets/leftarr.svg"
+import right from "/src/assets/rightarr.svg"
+import downl from "/src/assets/download.svg"
+import bell from "/src/assets/bell.svg"
+import spot from "/src/assets/spotify.svg"
+
 const TopPlay = () => (
-  <div>TopPlay</div>
+  <div className="flex gap justify-between items-center py-4">
+    <div className="flex gap-3 items-center">
+      <div className="rounded-full bg-[#000000b3] w-[32px] h-[32px] p-2"><img src={left} alt="" className="w-[16px] h-[16px]" /></div>
+      <div className="rounded-full bg-[#000000b3] w-[32px] h-[32px] p-2"><img src={right} alt="" className="w-[16px] h-[16px]" /></div>
+    </div>
+    <div className="flex items-center gap-2">
+      <button className="btn-white">Explore Premium</button>
+      <button className="btn-black"><img src={downl} alt="" className="w-[16px] h-[16px] inline-flex mr-2" /> Install App</button>
+      <div className="rounded-full bg-[#0000008a] w-[32px] h-[32px] p-2"><img src={bell} alt="" className="w-[16px] h-[16px]" /></div>
+      <div className="rounded-full bg-[#0000008a] w-[32px] h-[32px] p-2"><img src={spot} alt="" className="w-[16px] h-[16px]" /></div>
+    </div>
+  </div>
 );
 
 export default TopPlay;
